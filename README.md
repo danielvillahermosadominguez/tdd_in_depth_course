95e0b523b2fe9b9a4ed0380175fefb60bdadab49
Daniel Villahermosa <daniel.villahermosa@codurance.com>
2023-08-02 16:38:01 +0200

    Tasks: ATM - Unit tests

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # TDD in depth course
+
 ## Setup your test doubles
+
 We are going to use mockito https://site.mockito.org/
 
 We have added to the pom.xml the following dependency:
@@ -14,3 +16,16 @@ We have added to the pom.xml the following dependency:
 </dependency>
 
 ```
+
+## Task: ATM Unit tests
+
+- With the class Account, I think we are not verifying any behaviour. This is a class without
+  any behaviour. So the test hasn't any value.
+- Regarding the WelcomeScreen:
+    - I have used characterization test for the happy path, trying to understand the behaviour
+    - The edge cases cannot be tested (at least without changing the production code) because they
+      have infinite loops when there is an error.
+      NOTE: Maybe with awaitlaty we could test by using testing with thread. I didn't get it. But you
+      have some commented code.
+    - The code, sometimes is confuse, because we can see some methods which don't do what they say to do.
+
--- src/test/java/com/codurance/atm/WelcomeScreenShould.java
+++ src/test/java/com/codurance/atm/WelcomeScreenShould.java
@@ -1,10 +1,59 @@
 package com.codurance.atm;
 
+import com.codurance.atm.account.Account;
+import com.codurance.atm.account.AccountService;
+import com.codurance.atm.infrastructure.CliPrompt;
+import com.codurance.atm.screens.Screen;
+import com.codurance.atm.screens.ScreenEnum;
+import com.codurance.atm.screens.WelcomeScreen;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class WelcomeScreenShould {
     @Test
-    void foo() {
-        
+    void follow_the_process_of_login_and_returns_the_transaction_screen() {
+        //Arrange
+        CliPrompt cliPrompt = mock(CliPrompt.class);
+        Account expectedAccount = new Account("123456", 1000);
+        AccountService accountService = mock(AccountService.class);
+        WelcomeScreen welcomeScreen = new WelcomeScreen(cliPrompt, accountService);
+        when(cliPrompt.accountNumber()).thenReturn("123456");
+        when(accountService.findBy("123456", "789123")).thenReturn(expectedAccount);
+        when(cliPrompt.pin()).thenReturn("789123");
+
+        //Act
+        Screen screen = welcomeScreen.show();
+
+        //Assert
+        assertEquals(ScreenEnum.TRANSACTION, screen.screenName());
     }
+
+//    @Test
+//    void show_a_error_message_when_account_number_contain_chars() {
+//        //Arrange
+//        CliPrompt cliPrompt = mock(CliPrompt.class);
+//        Account expectedAccount = new Account("123456", 1000);
+//        AccountService accountService = mock(AccountService.class);
+//        WelcomeScreen welcomeScreen = new WelcomeScreen(cliPrompt, accountService);
+//        when(cliPrompt.accountNumber()).thenReturn("12345D");
+//        when(accountService.findBy("123456", "789123")).thenReturn(expectedAccount);
+//        when(cliPrompt.pin()).thenReturn("789123");
+//
+//        //Act
+//        Thread thread = new Thread(() -> welcomeScreen.show());
+//        thread.setDaemon(true);
+//        thread.start();
+//
+//        //Assert
+//        await()
+//            .timeout(10, TimeUnit.SECONDS)
+//            .untilAsserted(
+//                () -> verify(cliPrompt).promptGenericMessage("Account Number should only contain numbers\n")
+//            );
+//
+//        thread.interrupt();
+//    }
 }
